3305b430d78e309ffffc4b57cdad2e5359f8fdf3
Bernardo Vieira Santos <99152132+BeVieira@users.noreply.github.com>
2024-05-02 15:40:43 -0300

    Valida completa

--- src/Model/main.java
+++ src/Model/main.java
@@ -1,12 +1,11 @@
-//d/Visual Studio Projects/Eclipse/2220502_2212759_2210814
 package Model;
 
 public class main {
-
 	public static void main(String[] args) {
 		Jogador P1 = new Jogador("P1");
-		P1.tabuleiro.setCasa(new Coordenada(6,0), 1);
+		//P1.tabuleiro.setCasa(new Coordenada(6,0), 1);
 		P1.InserirEmbarcacao(new Coordenada(4,1), new Couracado());
+		P1.InserirEmbarcacao(new Coordenada(4,1), new Cruzador());
 		P1.tabuleiro.ExibeTabuleiro();
 	}
 }
