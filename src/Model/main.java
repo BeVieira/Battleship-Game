8d9d43f76a25b0326afc827843fc4ed25542efd4
Bernardo Vieira Santos <99152132+BeVieira@users.noreply.github.com>
2024-05-02 15:53:55 -0300

    Valida Posição atualizada

--- src/Model/main.java
+++ src/Model/main.java
@@ -1,11 +1,12 @@
 package Model;
 
 public class main {
+
 	public static void main(String[] args) {
 		Jogador P1 = new Jogador("P1");
 		//P1.tabuleiro.setCasa(new Coordenada(6,0), 1);
-		P1.InserirEmbarcacao(new Coordenada(4,1), new Couracado());
-		P1.InserirEmbarcacao(new Coordenada(4,1), new Cruzador());
+		P1.InserirEmbarcacao(new Coordenada(0,0), new Couracado());
+		P1.InserirEmbarcacao(new Coordenada(14,14), new Couracado());
 		P1.tabuleiro.ExibeTabuleiro();
 	}
 }
