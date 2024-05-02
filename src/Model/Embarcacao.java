f4595dc18d86bfde369e07b2d1fc99b157df7ebb
Bernardo Vieira Santos <99152132+BeVieira@users.noreply.github.com>
2024-05-02 15:40:13 -0300

    Valida completa

--- src/Model/Embarcacao.java
+++ src/Model/Embarcacao.java
@@ -11,47 +11,143 @@ public class Embarcacao {
 		return tipo;
 	}
 	
-	//TO-DO validar posiçao da embarcaçao (Override no Hidroaviao)
-	public boolean ValidaPosicionar(Tabuleiro t, Coordenada c) {
-		int tam = this.getTipo();
-		
-		Coordenada aux = new Coordenada(c.getColuna(), c.getLinha());
-		int coluna_aux = aux.getColuna();
-		int linha_aux = aux.getLinha();
-		
-		//testar a coordenada anterior
-		aux.setColuna(coluna_aux-1);
-		if (t.getCasa(aux) == 0) {
-			//se a anterior tiver ok entra aqui e vai validar a parte toda de cima
-			aux.setLinha(linha_aux-1);
-			aux.setColuna(coluna_aux+1);
-			//Voltei a coordenada original
-			for (int i = 0; i < tam; i++) {
-				if (t.getCasa(aux) != 0)
-					return false;
-				aux.setColuna(coluna_aux+i);
-			}
+
+	public boolean ValidaCasa(Tabuleiro t, Coordenada c){
+		return t.getCasa(c) == 0;
+	}
+
+	public boolean ValidaLinha(Tabuleiro t, Coordenada c){
+		for (int i = 0, colunaTemp = c.getColuna(); i < this.getTipo(); i++){
+			c.setColuna(colunaTemp+i);
+			if (t.getCasa(c) != 0)
+				return false;
 		}
-			//parte de cima ok testando a de baixo agr
-			aux.setColuna(coluna_aux);
-			aux.setLinha(linha_aux+1);
-			if (t.getCasa(aux) == 0) {
-				for (int i = 0; i < tam;  i++) {
-					if (t.getCasa(aux) != 0)
+		return true;
+	}
+
+
+	public boolean ValidaPosicionar(Tabuleiro t, Coordenada c) {
+			int tam = this.getTipo();
+			Coordenada aux = new Coordenada(c.getColuna(), c.getLinha());
+			int colunaOriginal = aux.getColuna();
+			int colunaAnterior = colunaOriginal - 1;
+			int colunaPosterior = colunaOriginal + tam; 
+
+			int linhaOriginal = aux.getLinha();
+			int linhaAnterior = linhaOriginal - 1;
+			int linhaSeguinte = linhaOriginal + 1;
+
+			//Caso casas onde a embarcação será inserida estar ocupada
+			for (int i = 0, colunaTemp = c.getColuna(); i < this.getTipo(); i++){
+				aux.setColuna(colunaTemp+i);
+				if (t.getCasa(c) != 0)
+					return false;
+			}
+			aux.setColuna(colunaOriginal);
+		
+			//Caso selecione casa ocupada (por tiro ou embarcação)
+			if (t.getCasa(c) != 0)
+				return false;
+			
+			//Caso adjacência 
+			//Linha A
+			if (linhaOriginal == 0){
+				//Valida Abaixo
+				aux.setLinha(linhaSeguinte);
+				if (!ValidaLinha(t,aux))
+					return false;
+				aux.setLinha(linhaOriginal);
+
+				//Coluna 1
+				if (colunaOriginal == 0){
+					//Valida Depois
+					aux.setColuna(colunaPosterior);
+					if (!ValidaCasa(t,aux))
 						return false;
-					aux.setColuna(coluna_aux+i);
 				}
+
+				//Coluna 15
+				else if ((colunaOriginal + tam) >= 14){
+					//Valida Antes
+					aux.setColuna(colunaAnterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+				}
+				else{
+					//Valida Antes
+	                aux.setColuna(colunaAnterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+					//Valida Depois
+	                aux.setColuna(colunaPosterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+				}
+			}
+			//Linha O
+			else if (linhaOriginal == 14){
+				//Valida Acima
+	            aux.setLinha(linhaAnterior);
+				if (!ValidaLinha(t,aux))
+					return false;
+				aux.setLinha(linhaOriginal);
+
+				//Coluna 1
+				if (colunaOriginal == 0){
+					//Valida Depois
+	                aux.setColuna(colunaPosterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+				}
+				//Coluna 15
+				else if ((colunaOriginal + tam) == 14){
+					//Valida Antes
+	                aux.setColuna(colunaAnterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+				}
+				else{
+					//Valida Antes
+	                aux.setColuna(colunaAnterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+					//Valida Depois
+	                aux.setColuna(colunaPosterior);
+					if (!ValidaCasa(t,aux))
+						return false;
+				}
+			}
+			else {
+				//Valida Acima
+	            aux.setLinha(linhaAnterior);
+				if (!ValidaLinha(t,aux))
+					return false;
+				aux.setLinha(linhaSeguinte);
+				if (!ValidaLinha(t,aux))
+					return false;
+				aux.setLinha(linhaOriginal);
+				//Valida Antes
+	            aux.setColuna(colunaAnterior);
+	            if (!ValidaCasa(t,aux))
+	                return false;
+	            //Valida Depois
+	            aux.setColuna(colunaPosterior);
+	            if (!ValidaCasa(t,aux))
+	                return false;
 			}
 			return true;
 	}
 	
 	public boolean Posicionar(Tabuleiro tabuleiro, Coordenada coordenada) {
 		int colunaAux = coordenada.getColuna();
-		for (int i = 0; i < this.getTipo(); i++) {
-			coordenada.setColuna(colunaAux+i);
-			tabuleiro.setCasa(coordenada, this.getTipo());
+		if (ValidaPosicionar(tabuleiro,coordenada)) {
+			for (int i = 0; i < this.getTipo(); i++) {
+				coordenada.setColuna(colunaAux+i);
+				tabuleiro.setCasa(coordenada, this.getTipo());
+			}
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 }
