89c7c456764be630491a31ba035f4ada57e7ff52
Bernardo Vieira Santos <99152132+BeVieira@users.noreply.github.com>
2024-06-11 10:35:06 -0300

    Update Embarcacao Valida (Ler comentario)
    Mudar lógica da view para 0 ate 14

--- src/Model/Embarcacao.java
+++ src/Model/Embarcacao.java
@@ -14,14 +14,15 @@ public class Embarcacao {
 	public boolean ValidaPosicionar(Tabuleiro t, Coordenada c) {
 	        int linha = c.getLinha();
 	        int coluna = c.getColuna();
-	        
+	        Coordenada novaC = new Coordenada(0,0);
+	        if (coluna + this.getTipo() > 14) // Tem que mudar a logica da view (0 até 14)
+	        	return false;
 	        for (int n = 0; n < this.getTipo(); n++) {
 	        	for (int i = -1; i <= 1; i++) {
 	                	for (int j = -1; j <= 1; j++) {
 	                    		int novaLinha = linha + i;
 	                    		int novaColuna = coluna + j;
 	                    		if (novaLinha >= 0 && novaLinha <= 14 && novaColuna >= 0 && novaColuna <= 14) {
-		                       		Coordenada novaC = new Coordenada(0,0);
 			                        novaC.setLinha(novaLinha);
 			                        novaC.setColuna(novaColuna);
 			                    	if (t.getCasa(novaC) != 0)
