a85d515c04aaa8e869226ee1efc1bbb5dacdbe06
Bernardo Vieira Santos <99152132+BeVieira@users.noreply.github.com>
2024-06-12 12:55:33 -0300

    Update Embarcacao.java

--- src/Model/Embarcacao.java
+++ src/Model/Embarcacao.java
@@ -29,7 +29,7 @@ public class Embarcacao {
 			                            return false;
 	                    		}
 	                	}
-	        	linha += n;
+	        	coluna += n;
 	        	}
 	        }
 	        return true; 
